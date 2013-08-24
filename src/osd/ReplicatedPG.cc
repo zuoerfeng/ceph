17c2770a6981de27de0bffc7e01109e1598d0503
Sage Weil <sage@inktank.com>
2013-08-23 21:34:28 -0700

    osd/ReplicatedPG: drop src_obc.clear() calls
    These are all about to go out of scope; no need to clear them
    explicitly.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -836,7 +836,6 @@ void ReplicatedPG::do_op(OpRequestRef op)
       dout(10) << "no src oid specified for multi op " << osd_op << dendl;
       osd->reply_op_error(op, -EINVAL);
     }
-    src_obc.clear();
     return;
   }
 
@@ -867,7 +866,6 @@ void ReplicatedPG::do_op(OpRequestRef op)
 	  src_obc[clone_oid] = sobc;
 	  continue;
 	}
-	src_obc.clear();
 	return;
       } else {
 	continue;
@@ -909,7 +907,6 @@ void ReplicatedPG::do_op(OpRequestRef op)
     if (oldv != eversion_t()) {
       dout(3) << "do_op dup " << ctx->reqid << " was " << oldv << dendl;
       delete ctx;
-      src_obc.clear();
       if (already_complete(oldv)) {
 	osd->reply_op_error(op, 0, oldv);
       } else {
@@ -985,7 +982,6 @@ void ReplicatedPG::do_op(OpRequestRef op)
   if (result == -EAGAIN) {
     // clean up after the ctx
     delete ctx;
-    src_obc.clear();
     return;
   }
 
@@ -993,7 +989,6 @@ void ReplicatedPG::do_op(OpRequestRef op)
   if (ctx->delta_stats.num_bytes > 0 &&
       pool.info.get_flags() & pg_pool_t::FLAG_FULL) {
     delete ctx;
-    src_obc.clear();
     osd->reply_op_error(op, -ENOSPC);
     return;
   }
@@ -1038,7 +1033,6 @@ void ReplicatedPG::do_op(OpRequestRef op)
     reply->add_flags(CEPH_OSD_FLAG_ACK | CEPH_OSD_FLAG_ONDISK);
     osd->send_message_osd_client(reply, m->get_connection());
     delete ctx;
-    src_obc.clear();
     return;
   }
 

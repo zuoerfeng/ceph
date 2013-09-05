linuxbox2/linuxbox-ceph

osdc/Objecter: whitespace
Signed-off-by: Matt Benjamin <matt@linuxbox.com>

src/osdc/Objecter.h
@@ -7,9 +7,9 @@
  *
  * This is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
- * License version 2.1, as published by the Free Software 
+ * License version 2.1, as published by the Free Software
  * Foundation.  See file COPYING.
- * 
+ *
  */
 
 #ifndef CEPH_OBJECTER_H
@@ -625,15 +625,15 @@ struct ObjectOperation {
 
 
 class Objecter {
- public:  
+ public:
   Messenger *messenger;
   MonClient *monc;
   OSDMap    *osdmap;
   CephContext *cct;
 
   bool initialized;
- 
- private:
+
+private:
   tid_t last_tid;
   int client_inc;
   uint64_t max_linger_id;
@@ -1310,9 +1310,9 @@ private:
   }
 
   tid_t getxattrs(const object_t& oid, const object_locator_t& oloc, snapid_t snap,
-             map<string,bufferlist>& attrset,
-	     int flags, Context *onfinish,
-	     eversion_t *objver = NULL, ObjectOperation *extra_ops = NULL) {
+		  map<string,bufferlist>& attrset,
+		  int flags, Context *onfinish,
+		  eversion_t *objver = NULL, ObjectOperation *extra_ops = NULL) {
     vector<OSDOp> ops;
     int i = init_ops(ops, 1, extra_ops);
     ops[i].op.op = CEPH_OSD_OP_GETXATTRS;
@@ -1329,9 +1329,10 @@ private:
 	          eversion_t *objver = NULL, ObjectOperation *extra_ops = NULL) {
     return read(oid, oloc, 0, 0, snap, pbl, flags | global_op_flags | CEPH_OSD_FLAG_READ, onfinish, objver);
   }
+
      
   // writes
-  tid_t _modify(const object_t& oid, const object_locator_t& oloc, 
+  tid_t _modify(const object_t& oid, const object_locator_t& oloc,
 		vector<OSDOp>& ops, utime_t mtime,
 		const SnapContext& snapc, int flags,
 	        Context *onack, Context *oncommit,
@@ -1415,7 +1416,7 @@ private:
 	      const SnapContext& snapc,
 	      utime_t mtime, int flags,
 	      uint64_t trunc_size, __u32 trunc_seq,
-              Context *onack, Context *oncommit,
+	      Context *onack, Context *oncommit,
 	      eversion_t *objver = NULL, ObjectOperation *extra_ops = NULL) {
     vector<OSDOp> ops;
     int i = init_ops(ops, 1, extra_ops);

zuoerfeng/ceph

src/osdc/Objecter.h
@@ -7,9 +7,9 @@
  *
  * This is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
- * License version 2.1, as published by the Free Software 
+ * License version 2.1, as published by the Free Software
  * Foundation.  See file COPYING.
- * 
+ *
  */
 
 #ifndef CEPH_OBJECTER_H
@@ -768,15 +768,15 @@ struct ObjectOperation {
 
 
 class Objecter {
- public:  
+ public:
   Messenger *messenger;
   MonClient *monc;
   OSDMap    *osdmap;
   CephContext *cct;
 
   bool initialized;
- 
- private:
+
+private:
   tid_t last_tid;
   int client_inc;
   uint64_t max_linger_id;
@@ -1514,9 +1514,9 @@ private:
   }
 
   tid_t getxattrs(const object_t& oid, const object_locator_t& oloc, snapid_t snap,
-             map<string,bufferlist>& attrset,
-	     int flags, Context *onfinish,
-	     version_t *objver = NULL, ObjectOperation *extra_ops = NULL) {
+		  map<string,bufferlist>& attrset,
+		  int flags, Context *onfinish,
+		  version_t *objver = NULL, ObjectOperation *extra_ops = NULL) {
     vector<OSDOp> ops;
     int i = init_ops(ops, 1, extra_ops);
     ops[i].op.op = CEPH_OSD_OP_GETXATTRS;
@@ -1533,9 +1533,10 @@ private:
 	          version_t *objver = NULL, ObjectOperation *extra_ops = NULL) {
     return read(oid, oloc, 0, 0, snap, pbl, flags | global_op_flags | CEPH_OSD_FLAG_READ, onfinish, objver);
   }
+
      
   // writes
-  tid_t _modify(const object_t& oid, const object_locator_t& oloc, 
+  tid_t _modify(const object_t& oid, const object_locator_t& oloc,
 		vector<OSDOp>& ops, utime_t mtime,
 		const SnapContext& snapc, int flags,
 	        Context *onack, Context *oncommit,
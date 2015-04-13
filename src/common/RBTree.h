1da58f99c9f50d9e09a08cb461851f08f8070776
Haomai Wang <haomaiwang@gmail.com>
2015-04-13 23:02:12 +0800

    Add RBTree
    Signed-off-by: Haomai Wang <haomaiwang@gmail.com>

---
+++ src/common/RBTree.h
@@ -0,0 +1,133 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2015 Haomai Wang <haomaiwang@gmail.com>
+ *
+ * Author: Haomai Wang <haomaiwang@gmail.com>
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+ */
+
+#ifndef CEPH_COMMON_RBTREE_H
+#define CEPH_COMMON_RBTREE_H
+
+#include <stddef.h>
+
+struct RBNode {
+  unsigned long  __rb_parent_color;
+  RBNode *rb_left;
+  RBNode *rb_right;
+  inline RBNode *parent() const { return (RBNode *)(__rb_parent_color & ~3); }
+  template<typename T>
+  inline T *get_container(int off) {
+    return reinterpret_cast<T*>((char*)(this) - off);
+  }
+} __attribute__((aligned(sizeof(long))));
+
+struct RBTree {
+  RBNode *rb_node;
+
+  RBTree(): rb_node(NULL) {}
+  void insert_color(RBNode *node);
+  void erase(RBNode *node);
+  void replace(RBNode *old, RBNode *n);
+
+  class Iterator {
+    friend class RBTree;
+   protected:
+    RBNode *cur;
+
+   public:
+    Iterator(): cur(NULL) {}
+    Iterator(RBNode *n): cur(n) {}
+    bool operator==(const Iterator& itr) const {
+      return cur == itr.cur;
+    }
+    bool operator!=(const Iterator& itr) const {
+      return cur != itr.cur;
+    }
+    RBNode* operator->() { return &(operator*()); }
+    RBNode& operator*() const { return *cur; }
+    Iterator& operator++() {
+      if (cur) {
+        /*
+         * If we have a right-hand child, go down and then left as far
+         * as we can.
+         */
+        if (cur->rb_right) {
+          cur = cur->rb_right;
+          while (cur->rb_left)
+            cur = cur->rb_left;
+        } else {
+          /*
+           * No right-hand children. Everything down and left is smaller than us,
+           * so any 'next' node must be in the general direction of our parent.
+           * Go up the tree; any time the ancestor is a right-hand child of its
+           * parent, keep going up. First time it's a left-hand child of its
+           * parent, said parent is our 'next' node.
+           */
+          RBNode *child = cur;
+          while ((cur = cur->parent()) && child == cur->rb_right)
+            child = cur;
+        }
+      }
+      return *this;
+    }
+    Iterator& operator--() {
+      if (cur) {
+        /*
+         * If we have a left-hand child, go down and then right as far
+         * as we can.
+         */
+        if (cur->rb_left) {
+          cur = cur->rb_left;
+          while (cur->rb_right)
+            cur = cur->rb_right;
+        } else {
+          /*
+           * No left-hand children. Go up till we find an ancestor which
+           * is a right-hand child of its parent.
+           */
+          RBNode *child = cur;
+          while ((cur = cur->parent()) && child == cur->rb_right)
+            child = cur;
+        }
+      }
+      return *this;
+    }
+  };
+  Iterator begin() {
+    RBNode *n = rb_node;
+    if (n) {
+      while (n->rb_left)
+        n = n->rb_left;
+    }
+    return Iterator(n);
+  }
+  Iterator last() {
+    RBNode *n = rb_node;
+    if (n) {
+      while (n->rb_right)
+        n = n->rb_right;
+    }
+    return Iterator(n);
+  }
+  Iterator end() {
+    return Iterator(NULL);
+  }
+
+  static inline void rb_link_node(RBNode *node, RBNode *parent, RBNode **rb_link)
+  {
+    node->__rb_parent_color = (unsigned long)parent;
+    node->rb_left = node->rb_right = NULL;
+    *rb_link = node;
+  }
+};
+
+#endif
---
+++ src/test/common/test_rbtree.cc
@@ -0,0 +1,179 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2015 Haomai Wang <haomaiwang@gmail.com>
+ *
+ * LGPL2.1 (see COPYING-LGPL2.1) or later
+ */
+
+#include <gtest/gtest.h>
+#include <time.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <map>
+#include "common/RBTree.h"
+#include "common/Cycles.h"
+
+#define NODES       100
+#define PERF_LOOPS  100000
+#define CHECK_LOOPS 100
+
+using namespace std;
+
+class TestRBTree : public ::testing::Test {
+  map<uint32_t, uint32_t> verify;
+
+ public:
+  struct TestNode {
+    RBNode rb;
+    uint32_t key;
+
+    /* following fields used for testing augmented rbtree functionality */
+    uint32_t val;
+    uint32_t augmented;
+  };
+
+  RBTree root;
+  TestNode nodes[NODES];
+  virtual void SetUp() {
+    for (int i = 0; i < NODES; i++) {
+      nodes[i].key = rand();
+      nodes[i].val = rand();
+    }
+  }
+
+  void lower_bound(uint64_t key) {
+    RBNode *n = root.rb_node, *parent = NULL;
+    TestNode *node = NULL;
+
+    while (n) {
+      parent = n;
+      node = n->get_container<TestNode>(offsetof(TestNode, rb));
+
+      if (key < node->key) {
+        n = n->rb_left;
+      } else if (key > node->key) {
+        n = n->rb_right;
+      } else {
+        parent = n;
+        break;
+      }
+    }
+
+    RBTree::Iterator it(parent);
+    if (node && key > node->key)
+      ++it;
+
+    map<uint32_t, uint32_t>::iterator verify_it = verify.lower_bound(key);
+    if (verify_it != verify.end()) {
+      ASSERT_EQ(verify_it->second, node->val);
+      ASSERT_EQ(verify_it->first, node->key);
+    } else {
+      ASSERT_TRUE(it == root.end());
+    }
+  }
+
+  void insert(TestNode *node)
+  {
+    RBNode **n = &root.rb_node, *parent = NULL;
+    uint32_t key = node->key;
+
+    while (*n) {
+      parent = *n;
+      if (key < parent->get_container<TestNode>(offsetof(TestNode, rb))->key)
+        n = &parent->rb_left;
+      else
+        n = &parent->rb_right;
+    }
+
+    root.rb_link_node(&node->rb, parent, n);
+    root.insert_color(&node->rb);
+
+    verify[key] = node->val;
+  }
+
+  inline void erase(TestNode *node)
+  {
+    root.erase(&node->rb);
+    verify.erase(node->key);
+  }
+
+  bool is_red(const RBNode &rb)
+  {
+    return !(rb.__rb_parent_color & 1);
+  }
+
+  int black_path_count(RBNode &rb)
+  {
+    RBNode *p = &rb;
+    int count;
+    for (count = 0; p; p = p->parent())
+      count += !is_red(*p);
+    return count;
+  }
+
+  void check(int nr_nodes)
+  {
+    int count = 0, blacks = 0;
+    uint32_t prev_key = 0;
+    map<uint32_t, uint32_t>::iterator verify_it = verify.begin();
+
+    for (RBTree::Iterator it = root.begin(); it != root.end(); ++it, ++verify_it) {
+      TestNode *node = it->get_container<TestNode>(offsetof(TestNode, rb));
+      ASSERT_TRUE(node->key >= prev_key);
+      ASSERT_FALSE(is_red(*it) && (!it->parent() || is_red(*it->parent())));
+      if (!count)
+        blacks = black_path_count(*it);
+      else
+        ASSERT_FALSE((!it->rb_left || !it->rb_right) && blacks != black_path_count(*it));
+      prev_key = node->key;
+      count++;
+      ASSERT_EQ(verify_it->second, node->val);
+      ASSERT_EQ(verify_it->first, prev_key);
+    }
+
+    ASSERT_TRUE(count == nr_nodes);
+    ASSERT_FALSE(count < (1 << black_path_count(*root.last())) - 1);
+  }
+};
+
+TEST_F(TestRBTree, Basic)
+{
+  int i, j;
+  uint64_t time1, time2, time;
+
+  std::cout << "rbtree testing";
+
+  srand(3588);
+
+  time1 = Cycles::rdtsc();
+
+  for (i = 0; i < PERF_LOOPS; i++) {
+    for (j = 0; j < NODES; j++)
+      insert(nodes + j);
+    for (j = 0; j < NODES; j++)
+      erase(nodes + j);
+  }
+
+  time2 = Cycles::rdtsc();
+  time = time2 - time1;
+
+  time = time / PERF_LOOPS;
+  std::cout << " -> " << time << " cycles" << std::endl;
+
+  for (i = 0; i < CHECK_LOOPS; i++) {
+    for (j = 0; j < NODES; j++) {
+      check(j);
+      insert(nodes + j);
+      lower_bound(rand());
+    }
+    for (j = 0; j < NODES; j++) {
+      check(NODES - j);
+      erase(nodes + j);
+      lower_bound(rand());
+    }
+    check(0);
+  }
+}
